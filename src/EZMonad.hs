7c58dee93269f58bf83e239587ab71642159f3c4
Asad Saeeduddin <masaeedu@gmail.com>
2020-02-05 21:11:47 -0500

    Formatting

--- src/EZMonad.hs
+++ src/EZMonad.hs
@@ -2,14 +2,14 @@ module EZMonad where
 
 import FCI
 
-type (::>>=) m = forall a b. m a -> (a -> m b) -> m b
-type Pure m = forall a. a -> m a
-type (::<$>) m = forall a b. (a -> b) -> m a -> m b
-type (::$>) m = forall a b. b -> m a -> m b
-type (::<*>) m = forall a b. m (a -> b) -> m a -> m b
-type LiftA2 m = forall a b c. (a -> b -> c) -> m a -> m b -> m c
-type (::*>) m = forall a b. m a -> m b -> m b
-type (::<*) m = forall a b. m a -> m b -> m a
+type (::>>=) m = forall a b  . m a -> (a -> m b) -> m b
+type Pure    m = forall a    . a -> m a
+type (::<$>) m = forall a b  . (a -> b) -> m a -> m b
+type (::$>)  m = forall a b  . b -> m a -> m b
+type (::<*>) m = forall a b  . m (a -> b) -> m a -> m b
+type LiftA2  m = forall a b c. (a -> b -> c) -> m a -> m b -> m c
+type (::*>)  m = forall a b  . m a -> m b -> m b
+type (::<*)  m = forall a b  . m a -> m b -> m a
 
 ezmonad :: forall m. (::>>=) m -> Pure m -> Inst (Monad m)
 ezmonad (>>=) pure = Monad applicative (>>=) (*>) pure error
@@ -37,4 +37,3 @@ ezmonad (>>=) pure = Monad applicative (>>=) (*>) pure error
 
   applicative :: Inst (Applicative m)
   applicative = Applicative functor pure (<*>) liftA2 (*>) (<*)
-
